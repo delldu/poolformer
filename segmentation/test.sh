7b905d309e6d9876d366f7c56cb60f1756533a0a
delldu <18588220928@163.com>
2021-12-12 18:19:39 +0800

    Update

--- segmentation/test.sh
+++ segmentation/test.sh
@@ -2,6 +2,6 @@
 
 export PYTHONPATH=..:$PYTHONPATH
 python test.py \
-    configs/sem_fpn/PoolFormer/fpn_poolformer_s24_ade20k_40k.py \
-    ../checkpoint/fpn_poolformer_s24_ade20k_40k.pth \
+    configs/sem_fpn/PoolFormer/fpn_poolformer_s12_ade20k_40k.py \
+    ../checkpoint/fpn_poolformer_s12_ade20k_40k.pth \
     --show-dir output \
